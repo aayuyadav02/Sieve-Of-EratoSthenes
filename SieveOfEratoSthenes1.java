4ea5feac320160583aaedeba7b54faae4b2b55b4
Aayush Yadav <142829005+aayuyadav02@users.noreply.github.com>
2024-09-18 23:34:32 +0530

    Add files via upload

---
+++ SieveOfEratoSthenes1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sieveoferatosthenes1;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author yadavaayush
+ */
+public class SieveOfEratoSthenes1 {
+
+    public static void main(String[] args) {
+       
+        boolean isPrime[] = seiveOfEratoSthenes(10);
+        for(int i=0;i<=10;i++){
+            System.out.println(i+" "+isPrime[i]);
+                    
+        }
+                
+    }
+    
+    static boolean[] seiveOfEratoSthenes(int n) {
+        
+        boolean isPrime[] = new boolean[n+1];
+        
+        Arrays.fill(isPrime, true);
+        
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        for(int i = 2; i*i <= n; i++){
+            
+            
+            for(int j = 2*i; j<=n; j += i){
+                isPrime[j]=false;
+            }
+        }
+        
+        
+        return isPrime;
+    }
+}
